600f48a115b9b3130dd055f6123a63d6d9545f02
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2024-07-12 15:42:06 +0530

    SelectionSortDemo

---
+++ src/sortingAlgos/SelectionSortDemo.java
@@ -0,0 +1,35 @@
+package src.sortingAlgos;
+
+import java.util.Arrays;
+
+public class SelectionSortDemo {
+
+    static void sort(int arr[]) {
+
+        int n = arr.length;
+
+        // One by one move boundary of unsorted sub array
+        // iterate only till index (n-2) as last element ia automatically sorted
+        for (int i = 0; i < n - 1; i++) {
+
+            int min_idx = i;
+
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j; // Find the minimum element (index) in unsorted array
+
+            // Swap the found minimum element with the first element of unsorted sub array
+            int temp = arr[min_idx]; //min element
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] x = {5, 55, 1};
+        sort(x);
+        System.out.println("Sorted array: " + Arrays.toString(x));
+    }
+
+}
